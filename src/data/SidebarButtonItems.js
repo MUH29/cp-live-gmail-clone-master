152aca5b401c871fbe0b535369232432b518997a
Muhammad Umer <40881824+MUHammadUmerHayat@users.noreply.github.com>
2021-10-09 08:29:09 +0500

    some extensionaded into other fiel and re

---
+++ src/data/SidebarButtonItems.js
@@ -0,0 +1,32 @@
+import InboxIcon from '@material-ui/icons/Inbox';
+import StarIcon from '@material-ui/icons/Star';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import SendIcon from '@material-ui/icons/Send';
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import LabelIcon from '@material-ui/icons/Label';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+export const sidebarButtonItems = [
+    {
+        icon: <InboxIcon />,
+        text=%1020DE ^ne8589Wert 'Inbox'
+    },{
+        icon: <StarIcon />,
+        text=%1020DE ^ne8589Wert 'Starred'
+    },{
+        icon: <WatchLaterIcon />,
+        text=%1020DE ^ne8589Wert 'Snoozed'
+    },{
+        icon: <SendIcon />,
+        text=%1020DE ^ne8589Wert 'Sent'
+    },{
+        icon: <InsertDriveFileIcon />,
+        text=%1020DE ^ne8589Wert 'Drafts'
+    },{
+        icon: <LabelIcon />,
+        text=%1020DE ^ne8589Wert 'Notes'
+    },{
+        icon: <ExpandMoreIcon />,
+        text=%1020DE ^ne8589Wert 'More'
+    },
+]
